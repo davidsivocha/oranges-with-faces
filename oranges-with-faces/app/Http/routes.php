a7d0fdb3a80f5d232a07ba4767462d5c43c1dffd
David Sivocha <david@sivocha.com>
2015-11-09 20:49:25 +0000

    Move Route Code into Controllers

--- oranges-with-faces/app/Http/routes.php
+++ oranges-with-faces/app/Http/routes.php
@@ -11,53 +11,6 @@
 |
 */
 
-$app->get('/', function () use ($app) {
-    return $app->welcome();
-});
-
-$app->post('/order', function (Request $request) use($app) {
-    $stripeKey = env('STRIPE_KEY');
-    $stripeSecret = env('STRIPE_SECRET');
-
-    \Stripe\Stripe::setApiKey($stripeSecret);
-
-    $token = $request->input('stripeToken');
-
-    $validateShipping = Validator::make(data, rules, messages, customAttributes);
-    if($validator->fails()){
-        throw new Exception();
-    }
-
-    $charge = \Stripe\Charge::create(array(
-        "amount" => Order::COST,
-        "currency" => Order::CURRENCY,
-        "source" => $token,
-        "description" => "An orange with a face"
-    ));
-
-    Order::create([
-        'charge_id'          => $charge->id,
-        'customer_id'        => $charge->customer,
-        'customer_email'     => $request->input('stripeEmail'),
-        'billing_name'       => $request->input('stripeBillingName'),
-        'billing_address_1'  => $request->input('stripeBillingAddressLine1'),
-        'billing_city'       => $request->input('stripeBillingAddressCity'),
-        'billing_state'      => $request->input('stripeBillingAddressState'),
-        'billing_country'    => $request->input('stripeBillingAddressCountry'),
-        'billing_zip'        => $request->input('stripeBillingAddressZip'),
-        'shipping_name'      => $request->input('stripeShippingName'),
-        'shipping_address_1' => $request->input('stripeShippingAddressLine1'),
-        'shipping_city'      => $request->input('stripeShippingAddressCity'),
-        'shipping_state'     => $request->input('stripeShippingAddressState'),
-        'shipping_country'   => $request->input('stripeShippingAddressCountry'),
-        'shipping_zip'       => $request->input('stripeShippingAddressZip'),
-        'total_cost'         => $charge->amount,
-        'currency'           => $charge->currency,
-        'status'             => Order::STATUS_CREATED
-    ]);
-    return $app->welcome();
-});
-
-$app->get('/admin', function () use ($app) {
-
-});
+$app->get('/', 'Controller@getIndex');
+$app->post('/order', 'Controller@postOrder');
+$app->get('/admin', 'Controller@getAdmin');
