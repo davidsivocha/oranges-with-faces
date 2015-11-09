f5e5d3cc0820ef6aed94d2cac57250cbe3beb5ba
David Sivocha <david@sivocha.com>
2015-11-09 12:06:34 +0000

    Prototype the Creation method in the route
    Adds a section where we validate the shipping address, then charge the
    card then create an order

--- oranges-with-faces/app/Http/routes.php
+++ oranges-with-faces/app/Http/routes.php
@@ -18,13 +18,39 @@ $app->get('/', function () use ($app) {
 $app->post('/charge', function () use($app) {
     $stripeKey = env('STRIPE_KEY');
     $stripeSecret = env('STRIPE_SECRET');
+
     \Stripe\Stripe::setApiKey($stripeSecret);
+
     $token = $_POST['stripeToken'];
+
+    $validateShipping = Validator::make(data, rules, messages, customAttributes);
+    if($validator->fails()){
+        throw new Exception();
+    }
+
     $charge = \Stripe\Charge::create(array(
         "amount" => 1000,
         "currency" => "gbp",
         "source" => $token,
         "description" => "Example charge"
     ));
+
+    Order::create([
+        'charge_id'          => $test,
+        'customer_id'        => $test,
+        'billing_name'       => $test,
+        'billing_address_1'  => $test,
+        'billing_city'       => $test,
+        'billing_state'      => $test,
+        'billing_country'    => $test,
+        'billing_zip'        => $test,
+        'shipping_name'      => $test,
+        'shipping_address_1' => $test,
+        'shipping_city'      => $test,
+        'shipping_state'     => $test,
+        'shipping_country'   => $test,
+        'shipping_zip'       => $test,
+        'total_cost'         => $test
+    ]);
     return $app->welcome();
 });
